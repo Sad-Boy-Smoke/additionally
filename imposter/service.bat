37badf038c878b2e0568ec2324c3128b525017e5
welstr1000@gmail.com <welsr1000@gmail.com>
2025-07-30 19:39:06 +0300

    marmok

--- imposter/service.bat
+++ imposter/service.bat
@@ -1,5 +1,5 @@
 @echo off
-set "LOCAL_VERSION=1.8.2"
+set "LOCAL_VERSION=1.8.3"
 
 :: External commands
 if "%~1"=="status_zapret" (
@@ -8,7 +8,11 @@ if "%~1"=="status_zapret" (
 )
 
 if "%~1"=="check_updates" (
-    call :service_check_updates soft
+    if not "%~2"=="soft" (
+        start /b service check_updates soft
+    ) else (
+        call :service_check_updates soft
+    )
     exit /b
 )
 
@@ -63,7 +67,7 @@ goto menu
 :service_status
 cls
 chcp 437 > nul
-for /f "tokens=3*" %%A in ('reg query "HKLM\System\CurrentControlSet\Services\zapret" /v zapret-discord-youtube 2^>nul') do echo Service strategy installed from "%%A %%B"
+for /f "tokens=2*" %%A in ('reg query "HKLM\System\CurrentControlSet\Services\zapret" /v zapret-discord-youtube 2^>nul') do echo Service strategy installed from "%%B"
 call :test_service zapret
 call :test_service WinDivert
 
@@ -266,7 +270,7 @@ for /f "delims=" %%A in ('powershell -command "(Invoke-WebRequest -Uri \"%GITHUB
 if not defined GITHUB_VERSION (
     echo Warning: failed to fetch the latest version. Check your internet connection. This warning does not affect the operation of zapret
     pause
-    if "%1"=="soft" exit /b 
+    if "%1"=="soft" exit 
     goto menu
 )
 
@@ -274,7 +278,7 @@ if not defined GITHUB_VERSION (
 if "%LOCAL_VERSION%"=="%GITHUB_VERSION%" (
     echo Latest version installed: %LOCAL_VERSION%
     
-    if "%1"=="soft" exit /b
+    if "%1"=="soft" exit 
     pause
     goto menu
 ) 
@@ -293,9 +297,10 @@ if /i "%CHOICE%"=="Y" (
 )
 
 
-if "%1"=="soft" exit /b
+if "%1"=="soft" exit 
 pause
 goto menu
+
 
 
 :: DIAGNOSTICS =========================
@@ -440,7 +445,7 @@ if exist "%gameFlagFile%" (
     set "GameFilter=1024-65535"
 ) else (
     set "GameFilterStatus=disabled"
-    set "GameFilter=0"
+    set "GameFilter=12"
 )
 exit /b
 
@@ -519,7 +524,7 @@ chcp 437 > nul
 cls
 
 set "listFile=%~dp0lists\ipset-all.txt"
-set "url=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/refs/heads/main/lists/ipset-all.txt"
+set "url=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/refs/heads/main/.service/ipset-service.txt"
 
 echo Updating ipset-all...
 
